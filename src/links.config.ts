ffafa48f931e17cfe1cf3a51078332a345287c29
Riley Draward <riley@trunk.io>
2025-05-31 09:10:46 -0400

    add links content collection

---
+++ src/links.config.ts
@@ -0,0 +1,21 @@
+import { defineCollection, z } from "astro:content";
+
+import { file } from "astro/loaders";
+
+const links = defineCollection({
+  loader: file("data/links.json"),
+  schema: z.object({
+    title: z.string(),
+    href: z.string().url(),
+    date: z.string().date(),
+    type: z.enum([
+      "case-study",
+      "light-hearted",
+      "editorial",
+      "guide",
+      "announcement",
+    ]),
+  }),
+});
+
+export const collections = { links };
