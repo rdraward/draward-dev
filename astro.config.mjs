ee351c7ff7dd626ba10d283126f1b8a1c8f24889
Riley Draward <riley@gadget.dev>
2026-05-22 17:07:11 -0400

    remove tailwindcss, split out components (#10)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import tailwindcss from '@tailwindcss/vite';
-
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://draward.dev',
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  site: 'https://draward.dev'
 });
